ed032e56856fb9ecc36e8867f2d0cd56f7011bb2
Blake A. Long <blake.a.long@gmail.com>
2020-03-19 22:12:28 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Add your code here
+const formatData = {
+    name: 'Steve',
+    email: 'steve@steve.com'
+}
+
+const submitData = (name, email) => {
+    return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify(formatData)
+    })
+    .then(response => response.json())
+    .then(response => { 
+        const body = document.body
+        const h1 = document.createElement('h1')
+        const h2 = document.createElement('h2')
+        h1.innerHTML = response.id
+        h2.innerHTML = response.name
+        body.appendChild(h1)
+        body.appendChild(h2)
+    })
+    .catch((error) => {
+        const body = document.body
+        const h1 = document.createElement('h1')
+        const h2 = document.createElement('h2')
+        h1.innerHTML = error.message
+        h2.innerHTML = error.code
+        body.appendChild(h1)
+        body.appendChild(h2)
+        console.log(error.message)
+        console.log(error)
+    })
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    submitData()
+})
+
